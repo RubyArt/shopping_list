5a0b31261d0f9d82c09b3c9a436ade60fcc5c999
TheMoniulla <skrobis.monika@gmail.com>
2017-06-17 01:07:52 +0200

    refactoring

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,10 +1,14 @@
 class Group < ActiveRecord::Base
-  after_destroy {|group| Membership.destroy(group.memberships.pluck(:id)) }
+  after_destroy { |group| Membership.destroy(group.memberships.pluck(:id)) }
 
   has_many :memberships, inverse_of: :group
   has_many :users, through: :memberships
   belongs_to :owner, class_name: 'User'
 
   validates :name, :users, :owner_id, presence: true
-  validates :name, uniqueness: { scope: :owner_id, message: 'This group already exists!' }
+  validates :name, uniqueness: {scope: :owner_id, message: 'This group already exists!'}
+
+  def owner?(user)
+    owner_id == user.id
+  end
 end
