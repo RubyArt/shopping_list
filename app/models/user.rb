f27b0116e8e54b41467d5b47aad3da7d839d2e9e
TheMoniulla <skrobis.monika@gmail.com>
2017-06-18 14:33:55 +0200

    Add first_name and last_name to Users, users views

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   has_many :groups, through: :memberships
   has_many :memberships, inverse_of: :user
   has_many :owned_groups, class_name: 'Group', foreign_key: :owner_id
+
+  validates :email, :first_name, :last_name, presence: true
 end
