88e0f5a528005bf1ceaf33a16301e2678f34ff38
TheMoniulla <skrobis.monika@gmail.com>
2017-06-20 21:59:35 +0200

    Main menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def controller?(*controller)
+    controller.include?(params[:controller])
+  end
+
+  def main_menu_link(name, path, active, options = {})
+    content_tag(:li, class: active ? 'active' : '') do
+      link_to name, path, options
+    end
+  end
 end
