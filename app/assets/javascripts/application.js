6de57b300bc989aa7325fb729965a45288e90073
TheMoniulla <skrobis.monika@gmail.com>
2017-06-24 23:20:43 +0200

    add react

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,9 @@
 //= require jquery-ui
 //= require twitter/bootstrap
 //= require turbolinks
+//= require react
+//= require react_ujs
+//= require components
 //= require_tree .
 
 function complete_check_handler(e) {
